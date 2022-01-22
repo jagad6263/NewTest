7d186a82d39ccca2783f3838fb476a6f7d772217
jagad6263 <jagad@live.in>
2022-01-22 17:33:07 -0500

    this is my 5t5h commit

---
+++ src/test1 - Copy.java
@@ -0,0 +1,25 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		
+
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		System.out.println("sdkjbv");
+		
+	}
+
+}
